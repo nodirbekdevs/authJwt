74731b920dff9990ef8ad250810b92b195ac1b57
Nodir <93400178+nodirbekdevs@users.noreply.github.com>
2021-11-08 18:51:27 +0500

    Update serializers.py

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -4,27 +4,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from users.serializers import UserSerializer
 
-# class UserSerializer(ModelSerializer):
-#     name = SerializerMethodField(read_only=True)
-#     _id = SerializerMethodField(read_only=True)
-#     is_admin = SerializerMethodField(read_only=True)
-#
-#     class Meta:
-#         model = User
-#         fields = ['id', '_id', 'username', 'email', 'name', 'is_admin']
-#
-#     def get__id(self, obj):
-#         return obj.id
-#
-#     def get_is_admin(self, obj):
-#         return obj.is_staff
-#
-#     def get_name(self, obj):
-#         name = obj.first_name
-#         if name == '':
-#             name = obj.email
-#         return name
-
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
